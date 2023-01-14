9e2150c8460297bbad72281ec77d61f1d6170440
jordan <jordan@localhost.localdomain>
2023-01-14 13:56:17 -0500

    More tinker

--- fedora.sh
+++ fedora.sh
@@ -4,14 +4,8 @@ sudo dnf update
 sudo dnf install dnf-plugins-core
 
 # font
-old=$(pwd)
-mkdir -p ~/.fonts
-cd ~/.fonts
-curl -fLo Hack.zip \
-    https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
-unzip Hack.zip
-rm Hack.zip
-cd $old
+curl -fLo ~/.fonts/"Hack Nerd Font Complete.otf" \
+    https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf?raw=true
 
 # set up configs
 mkdir -p ~/.config/ ~/.vim-sess
@@ -43,15 +37,24 @@ sudo dnf install -y \
     dunst \
     thunar \
     bluez \
+    arc-theme \
+    arandr \
+    gnome-control-center \
     NetworkManager NetworkManager-wifi network-manager-applet \
     brave-browser \
     lxappearance \
     lxsession \
-    `# tools` \
+    `# dev` \
     zsh \
+    vim \
+    tmux \
+    htop \
+    xclip \
     alacritty \
     neovim fzf ripgrep python3-neovim nodejs \
     docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
 
 # finish nvim
 sudo npm install yarn -g
@@ -62,8 +65,16 @@ nvim --headless +PlugInstall +qall
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# alacritty default
 sudo update-alternatives --install \
     /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
+
+# rofi stuff
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi
+./setup.sh
+cd ..
+rm -r rofi
 
 sudo systemctl enable sddm.service
 sudo systemctl set-default graphical.target
