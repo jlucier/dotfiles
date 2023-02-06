7391f6b612cbb71daf0ad6bde111a2d004d1ad5c
Jordan Lucier <jordan@perch.fit>
2023-02-06 08:47:46 -0500

    Fix font link

--- fedora/install.sh
+++ fedora/install.sh
@@ -94,7 +94,7 @@ install_docker() {
 
 fonts() {
     curl -fLo ~/.fonts/"JetBrainsMonoNL Nerd Font Complete.ttf" --create-dirs \
-        https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/complete/JetBrains%20Mono%20NL%20Regular%20Nerd%20Font%20Complete.ttf?raw=true
+	    https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/complete/JetBrains%20Mono%20NL%20Nerd%20Font%20Complete%20Regular.ttf?raw=true
 }
 
 dotconfig() {
