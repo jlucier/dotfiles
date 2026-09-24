3395ae55bf907c1efce5dd42aea9a0cc7774c205
Jordan Lucier <jordan@jordanlucier.com>
2026-09-24 07:47:06 -0400

    Trying transparent for fun

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -84,6 +84,7 @@ require("lazy").setup({
     priority = 1000,
     config = function()
       require("kanagawa").setup({
+        transparent = true,
         colors = {
           theme = {
             all = {
